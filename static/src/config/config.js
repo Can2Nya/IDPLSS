8a5e58da82568ffb33908417c32271e13aed1207
Ddragonever <ddragonever@gmail.com>
2016-10-02 12:14:36 +0800

    change font confirm url

--- static/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {};
-config.qiniu = "http://obowq22u8.bkt.clouddn.com";
-config.logo = config.qiniu+"/images/logo.png";
-config.logos = config.qiniu+"/images/slogo.png";
-config.logomid = config.qiniu+"/images/mid.png";
-config.line = config.qiniu+"/images/line.png";
-config.dot = config.qiniu+"/images/dot.png";
-
-export default config;
